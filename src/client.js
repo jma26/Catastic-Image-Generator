922308856a2876123b99189acce182717da72d3e
Jesse Ma <jma26code@gmail.com>
2025-07-28 23:18:57 -0700

    Return data payload successfully from dev server - client.js

--- src/client.js
+++ src/client.js
@@ -1,21 +1,26 @@
 const button = document.getElementsByClassName('hero-content__button')[0];
 
 button.addEventListener('click', () => {
-  console.log('Button clicked!');
   fetchImage();
 });
 
 async function fetchImage() {
   console.log('Fetching image...');
-  fetch('/api/cat-image')
+  fetch(`${process.env.API_HOST}/api/cat-image`)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    console.log('Response received', response.json());
+    return response.json();
   })
   .then(data => {
+    if (!data || data.length === 0) {
+      throw new Error('Data payload not received');
+    }
     console.log('This is the payload', data);
+    const imageUrl = data.url;
+    const imageHeight = data.height;
+    const imageWidth = data.width;
   })
   .catch(error => {
     console.error('Error', error);
