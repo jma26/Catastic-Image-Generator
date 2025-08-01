64ef3b56985cf7de7823b4ff46a33d1b7f9f74ee
Jesse Ma <jma26code@gmail.com>
2025-08-01 16:59:35 -0700

    Animate cat eyes in hero to follow mouse cursor

--- src/client.js
+++ src/client.js
@@ -69,3 +69,28 @@ function toggleModal() {
   document.body.classList.toggle('modal--open');
   modal.classList.toggle('modal--hidden');
 }
+
+document.addEventListener('mousemove', (event) => {
+  const pupils = document.querySelectorAll('.eye-pupil');
+
+  pupils.forEach(pupil => {
+    const eye = pupil.parentElement;
+    const eyeRect = eye.getBoundingClientRect();
+
+    const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+    const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+
+    const diffX = event.clientX - eyeCenterX;
+    const diffY = event.clientY - eyeCenterY;
+
+    const maxPupilMove = (eyeRect.width / 2) - (pupil.offsetWidth / 2);
+    const distance = Math.sqrt(diffX * diffX + diffY * diffY);
+
+    if (distance > maxPupilMove) {
+      const ratio = maxPupilMove / distance;
+      pupil.style.transform = `translate(${diffX * ratio}px, ${diffY * ratio}px)`;
+    } else {
+      pupil.style.transform = `translate(${diffX}px, ${diffY}px)`;
+    }
+  });
+});
